5da65ecf45f095cd26f25e0deaef0e768ada3db7
riikuid <fahmibambe@gmail.com>
2023-03-17 14:19:33 +0700

    floating button total

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:serpismotor/feature/totalscreen.dart';
 import 'package:serpismotor/utils/colors.dart';
 import 'package:serpismotor/views/components/body.dart';
-import 'package:badges/badges.dart' as badges;
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:serpismotor/cart/item_model.dart';
-import 'package:serpismotor/provider/cartprovider.dart';
-import 'package:serpismotor/database/db_helper.dart';
-import 'package:serpismotor/cart/cart_model.dart';
-import 'package:serpismotor/feature/totalscreen.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -16,46 +9,70 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: backgroundColor,
-        actions: [
-          badges.Badge(
-            badgeContent: Consumer<CartProvider>(
-              builder: (context, value, child) {
-                return Text(
-                  value.getCounter().toString(),
-                  style: const TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.bold),
-                );
-              },
-            ),
-            position: badges.BadgePosition.custom(start: 30, bottom: 30),
-            child: IconButton(
+     appBar: buildAppbar(),
+     backgroundColor: primaryColor,
+     body: Body(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Stack(
+        children: [
+          Positioned(
+            bottom: 20, // mengatur jarak dari bawah layar
+            left: 20, // mengatur jarak dari sisi kiri layar
+            right: 20, // mengatur jarak dari sisi kanan layar
+            child: FloatingActionButton(
               onPressed: () {
                 Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const CartScreen()));
+                  context,
+                  // DetailPage adalah halaman yang dituju
+                  MaterialPageRoute(
+                      builder: (context) => CartScreen()
+                  ),
+                );
               },
-              icon: const Icon(Icons.save, color: Colors.black,),
+              child: Container(
+                height: 70, // mengatur tinggi container
+                decoration: BoxDecoration(
+                  color: Color(0xffFFC646),
+                  borderRadius: BorderRadius.circular(100),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 1,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Total Biaya",
+                        style: TextStyle(fontSize: 16, color: Colors.black),
+                      ),
+                      Text(
+                        "Rp. 50.000",
+                        style: TextStyle(fontSize: 16, color: Colors.black),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
           ),
-          const SizedBox(
-            width: 20.0,
-          ),
         ],
-      ),
-
-      backgroundColor: backgroundColor,
-      body: Body(),
+      )
     );
   }
 
-  AppBar buildAppbar() {
+  AppBar buildAppbar(){
     return AppBar(
+
       elevation: 0,
       backgroundColor: backgroundColor,
+
     );
   }
 }
