a9f7a3527c1dcdad917b541c6f288067b31c5fa2
akmal <akmalfaditya@gmail.com>
2023-03-17 08:26:26 +0700

    fitur add dan total

---
+++ lib/database/db_helper.dart
@@ -0,0 +1,59 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+import 'dart:io' as io;
+import 'package:serpismotor/cart/cart_model.dart';
+
+class DBHelper{
+  static Database? _database;
+  
+  Future<Database?> get database async{
+    if(_database != null){
+      return _database;
+    }
+    _database = await initDatabase();
+    return null;
+  }
+  
+  initDatabase()async{
+    io.Directory directory = await getApplicationDocumentsDirectory();
+    String path = join(directory.path, 'cart.db');
+    var db = await openDatabase(path, version: 1, onCreate: _onCreate);
+    return db;
+  }
+  
+  // create tabel database
+  
+  _onCreate(Database db, int version) async {
+    await db.execute('CREATE TABLE cart(id INTEGER PRIMARY KEY, productId VARCHAR UNIQUE, productName TEXT, initialPrice INTEGER, productPrice INTEGER, quantity INTEGER, unitTag TEXT, image TEXT)');
+
+  }
+
+  //insert data ke tabel
+  Future<Cart> insert(Cart cart) async{
+    var dbClient = await database;
+    await dbClient!.insert('cart', cart.toMap());
+    return cart;
+
+  }
+
+  //get data
+  Future<List<Cart>> getCartList() async {
+    var dbClient = await database;
+    final List<Map<String, Object?>> queryResult = await dbClient!.query('cart');
+    return queryResult.map((result) => Cart.fromMap(result)).toList();
+
+  }
+  //update jumlah
+  Future<int> updateQuantity(Cart cart) async{
+    var dbClient = await database;
+    return await dbClient!.update('cart', cart.toMap(),
+        where: "productId = ?", whereArgs: [cart.productId]);
+  }
+  
+  //delete item
+  Future<int> deleteCartItem(int id) async{
+    var dbClient = await database;
+    return await dbClient!.delete('cart', where: 'id = ?', whereArgs: [id]);
+  }
+}
---
+++ lib/feature/isiserpis.dart
@@ -0,0 +1,180 @@
+import 'package:badges/badges.dart' as badges;
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:serpismotor/cart/item_model.dart';
+import 'package:serpismotor/provider/cartprovider.dart';
+import 'package:serpismotor/database/db_helper.dart';
+import 'package:serpismotor/cart/cart_model.dart';
+import 'package:serpismotor/feature/totalscreen.dart';
+
+class ProductList extends StatefulWidget {
+  const ProductList({Key? key}) : super(key: key);
+
+  @override
+  State<ProductList> createState() => _ProductListState();
+}
+
+class _ProductListState extends State<ProductList> {
+  DBHelper dbHelper = DBHelper();
+
+  List<Item> products = [
+    Item(
+        name: 'Servis Rutin',
+        unit: 'pcs',
+        price: 65000,
+        image: 'assets/images/tunjungan.jpg'),
+    Item(
+        name: 'Ganti oli mesin dan filter oli',
+        unit: 'pcs',
+        price: 250000,
+        image: 'assets/images/tunjungan.jpg'),
+    Item(
+        name: 'Ganti filter udara',
+        unit: 'pcs',
+        price: 100000,
+        image: 'assets/images/tunjungan.jpg'),
+    Item(
+        name: 'Ganti busi',
+        unit: 'pcs',
+        price: 25000,
+        image: 'assets/images/tunjungan.jpg'),
+    Item(
+        name: 'Ganti kampas rem',
+        unit: 'pcs',
+        price: 125000,
+        image: 'assets/images/tunjungan.jpg'),
+    Item(
+        name: 'Ganti aki',
+        unit: 'pcs',
+        price: 250000,
+        image: 'assets/images/tunjungan.jpg'),
+    Item(
+        name: 'Service rantai',
+        unit: 'pcs',
+        price: 150000,
+        image: 'assets/images/tunjungan.jpg'),
+    Item(
+        name: 'Ganti ban dan balancing roda',
+        unit: 'pcs',
+        price: 500000,
+        image: 'assets/images/tunjungan.jpg'),
+    Item(
+        name: 'Service rem',
+        unit: 'pcs',
+        price: 65000,
+        image: 'assets/images/tunjungan.jpg'),
+    Item(
+        name: 'Rem blong:',
+        unit: 'pcs',
+        price: 125000,
+        image: 'assets/images/tunjungan.jpg'),
+  ];
+
+  //List<bool> clicked = List.generate(10, (index) => false, growable: true);
+  @override
+  Widget build(BuildContext context) {
+    final cart = Provider.of<CartProvider>(context);
+    void saveData(int index) {
+      dbHelper
+          .insert(
+        Cart(
+          id: index,
+          productId: index.toString(),
+          productName: products[index].name,
+          initialPrice: products[index].price,
+          productPrice: products[index].price,
+          quantity: ValueNotifier(1),
+          unitTag: products[index].unit,
+          image: products[index].image,
+        ),
+      )
+          .then((value) {
+        cart.addTotalPrice(products[index].price.toDouble());
+        cart.addCounter();
+        print('Product Added to cart');
+      }).onError((error, stackTrace) {
+        print(error.toString());
+      });
+    }
+
+
+
+    return Expanded(
+      child: ListView.builder(
+          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 8.0),
+          shrinkWrap: true,
+          itemCount: products.length,
+
+          itemBuilder: (context, index) {
+            return Card(
+              color: Colors.blueGrey.shade200,
+              elevation: 5.0,
+              child: Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Image(
+                      height: 80,
+                      width: 80,
+                      image: AssetImage(products[index].image.toString()),
+                    ),
+                    SizedBox(
+                      width: 130,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(
+                            height: 5.0,
+                          ),
+                          RichText(
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 1,
+                            text: TextSpan(
+                                text: '',
+                                style: TextStyle(
+                                    color: Colors.blueGrey.shade800,
+                                    fontSize: 16.0),
+                                children: [
+                                  TextSpan(
+                                      text:
+                                          '${products[index].name.toString()}\n',
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold)),
+                                ]),
+                          ),
+
+                          RichText(
+                            maxLines: 1,
+                            text: TextSpan(
+                                text: 'Rp. ',
+                                style: TextStyle(
+                                    color: Colors.blueGrey.shade800,
+                                    fontSize: 16.0),
+                                children: [
+                                  TextSpan(
+                                      text:
+                                          '${products[index].price.toString()}\n',
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold)),
+                                ]),
+                          ),
+                        ],
+                      ),
+                    ),
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blueGrey.shade900),
+                        onPressed: () {
+                          saveData(index);
+                        },
+                        child: const Text('+')),
+                  ],
+                ),
+              ),
+            );
+          }),
+    );
+  }
+}
